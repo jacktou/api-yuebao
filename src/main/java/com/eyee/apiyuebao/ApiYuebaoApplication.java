bd13d9bb8b0f6242c8f5b1f259c95c2abe89d376
jqljh99 <jack@eyee.com>
2018-10-30 16:31:45 +0800

    init

---
+++ src/main/java/com/eyee/apiyuebao/ApiYuebaoApplication.java
@@ -0,0 +1,12 @@
+package com.eyee.apiyuebao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiYuebaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiYuebaoApplication.class, args);
+	}
+}
