707d883dc72f4f3cd4f3e463cf9f561562168222
jqljh99 <jack@eyee.com>
2018-10-30 16:48:47 +0800

    test

--- src/main/java/com/eyee/apiyuebao/ApiYuebaoApplication.java
+++ src/main/java/com/eyee/apiyuebao/ApiYuebaoApplication.java
@@ -2,11 +2,17 @@ package com.eyee.apiyuebao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ApiYuebaoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiYuebaoApplication.class, args);
 	}
+
+	@RequestMapping("/api/test")
+	public String testDemo(){return "hello world";}
 }
