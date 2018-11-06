981af66d3f70f3af6e04e8f6a2c7cc82116b667a
jqljh99 <jack@eyee.com>
2018-11-06 16:06:29 +0800

    init config environment

---
+++ environment.groovy
@@ -0,0 +1,128 @@
+environments {
+    local {
+        project {
+            env = "local"
+
+        }
+        log{
+            active = "dev"
+        }
+        server{
+            port=7070
+        }
+
+
+
+        db {
+            url = "jdbc:mysql://rm-bp1lx810m6218roaz2o.mysql.rds.aliyuncs.com:3306/yuebao_local?useUnicode=true&characterEncoding=utf-8&useSSL=false&autoReconnect=true"
+            username = "eyee_test"
+            password = "Eyee@934"
+            driverClassName = "com.mysql.jdbc.Driver"
+
+
+        }
+
+        undertow{
+            accessLogDirectory="../logs"
+        }
+
+        debug{
+            pay=true
+        }
+
+
+
+    }
+    dev {
+        project {
+            env = "dev"
+
+        }
+        log{
+            active = "dev"
+        }
+        server{
+            port=7070
+        }
+
+        db {
+            url = "jdbc:mysql://rm-bp1lx810m6218roaz2o.mysql.rds.aliyuncs.com:3306/yuebao_local?useUnicode=true&characterEncoding=utf-8&useSSL=false&autoReconnect=true"
+            username = "eyee_test"
+            password = "Eyee@934"
+            driverClassName = "com.mysql.jdbc.Driver"
+
+
+        }
+
+        undertow{
+            accessLogDirectory="../logs"
+        }
+
+        debug{
+            pay=true
+        }
+    }
+
+    test {
+        project {
+            env = "test"
+
+        }
+        log{
+            active = "prod"
+        }
+        server{
+            port=7070
+        }
+
+        db {
+            url = "jdbc:mysql://rm-bp1lx810m6218roaz2o.mysql.rds.aliyuncs.com:3306/yuebao_local?useUnicode=true&characterEncoding=utf-8&useSSL=false&autoReconnect=true"
+            username = "eyee_test"
+            password = "Eyee@934"
+            driverClassName = "com.mysql.jdbc.Driver"
+
+
+        }
+
+        undertow{
+            accessLogDirectory="../logs"
+        }
+
+        debug{
+            pay=true
+        }
+    }
+
+    production {
+        project {
+            env = "production"
+
+        }
+        log{
+            active = "prod"
+        }
+        server{
+            port=7070
+        }
+
+        db {
+            url = "jdbc:mysql://rm-bp1lx810m6218roaz2o.mysql.rds.aliyuncs.com:3306/yuebao_local?useUnicode=true&characterEncoding=utf-8&useSSL=false&autoReconnect=true"
+            username = "eyee_test"
+            password = "Eyee@934"
+            driverClassName = "com.mysql.jdbc.Driver"
+
+
+        }
+
+        undertow{
+            accessLogDirectory="../logs"
+        }
+
+        debug{
+            pay=false
+        }
+
+    }
+
+
+}
--- src/main/java/com/eyee/apiyuebao/ApiYuebaoApplication.java
+++ src/main/java/com/eyee/apiyuebao/ApiYuebaoApplication.java
@@ -2,6 +2,7 @@ package com.eyee.apiyuebao;
 
 import com.eyee.apiyuebao.model.ResponseBase;
 import com.eyee.apiyuebao.model.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@Slf4j
 public class ApiYuebaoApplication {
 
 	public static void main(String[] args) {
@@ -29,7 +31,7 @@ public class ApiYuebaoApplication {
 
 	@RequestMapping("/api/user/{id}")
 	public ResponseBase<User> getUser(@PathVariable("id") String id){
-
+        log.info("uuuuuuuuu");
 		return ResponseBase.succeeded().setData(User.builder().id(id).bulid());
 	}
 
